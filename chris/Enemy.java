077cb49f9d0e42bba559b088691d7fea92a0f42e
Ben <bescobar@iastate.edu>
2017-02-25 15:48:15 -0600

    the spider was added, code is getting really nasty now tho

---
+++ chris/Enemy.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.scene.input.KeyCode;
+
+public abstract class Enemy extends MovableObjects {
+
+	public Enemy(String imageName, int xSquare, int ySquare, int width, int height, int gl1x, int gl1y) {
+
+		super(imageName, xSquare, ySquare, width, height, gl1x, gl1y);
+
+	}
+
+	public void trackPlayer(double playerX, double playerY, final MovableObjects player) {
+
+		double xDistance = playerX - this.x;
+		double yDistance = playerY - this.y;
+
+		if (Math.abs(xDistance) < 20 && Math.abs(yDistance) < 20)
+			this.interact(player);
+
+//		if (Math.abs(xDistance) > Math.abs(yDistance)) {
+//			if (xDistance > 0)
+//				this.move(KeyCode.RIGHT);
+//			else if (xDistance < 0)
+//				this.move(KeyCode.LEFT);
+//		} else if (Math.abs(xDistance) < Math.abs(yDistance)) {
+//			if (yDistance > 0)
+//				this.move(KeyCode.DOWN);
+//			else if (yDistance < 0)
+//				this.move(KeyCode.UP);
+//		}
+
+	}
+
+	public void interact(final MovableObjects player) {
+		attack(player);
+	}
+
+	public abstract void attack(final MovableObjects player);
+
+}
