2e817c989d374e94976b53bd6e4236a0229b3358
jenny <jha@iastate.edu>
2017-02-25 15:38:26 -0600

    Item package updated incomplete

---
+++ workspace/hackisuproj/src/item/Potions.java
@@ -0,0 +1,23 @@
+//package item;
+//
+//import javafx.stage.Stage;
+//
+//public class Potions extends Items{
+//
+//	public Potions(String imageName, int xSquare, int ySquare, int width, int height) {
+//		super(imageName, xSquare, ySquare, width, height);
+//	}
+//
+//	@Override
+//	public void interact() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//}
